b3c1db355e8968d53695407c80c76771c507e42b
Noah Lorincz-Comi <noahlorinczcomi@gmail.com>
2025-11-22 17:23:30 -0500

    update

--- chatistician/server_functions.py
+++ chatistician/server_functions.py
@@ -24,6 +24,41 @@ def receive_msg(
             print(f"Error: {e}")
             break
 
+# function to run simulation (R script) from within chat
+def run_simulation(script, **kwargs):
+    cmd = [
+        f"Rscript {script} {kwargs}"
+    ]
+    subprocess.run(cmd)
+
+# function to parse messages before sending/running
+def server_msg_parser(
+    conn,
+    msg,
+    colored_server_name,
+    cmd_prefix='$',
+    sim_commands=['simulate', 'simulation'],
+    file_commands=['get', 'download']
+):
+    """
+    The idea is that some messages from the server 
+    will be chats, and others will be commands to
+    execute other commands/files. I want to make sure
+    I know what type of information the message intends
+    to send before sending
+    """
+    if msg[0] != cmd_prefix:
+        send_msg(conn, name, breakers)
+    elif msg[0] in sim_commands:
+        # else, user wants to perform a simulation. The
+        # message itself will contain the simulation
+        # parameters if they want to perform a simulation
+        kwargs = " ".join(msg.split()[3:])
+        run_simulation(script, kwargs)
+    elif msg[0] in file_commands:
+        # don't have a solution for this yet
+        pass
+
 # send message
 def send_msg(
     conn,
@@ -33,6 +68,14 @@ def send_msg(
     while True:
         try:
             msg = input(f"{colored_server_name} ")
+            # make sure we know what message is trying to sent
+            server_msg_parser(
+                conn,
+                msg,
+                colored_server_name
+            )
+            # check if message is for an R script
+            if msg[0] == ':' and 
             conn.sendall(msg.encode())
             if msg.lower() in breakers:
                 conn.close()
