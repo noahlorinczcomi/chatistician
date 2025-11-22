fe349bbb3521b97f1b1e2e6146f659ee2f6f730e
Noah Lorincz-Comi <noahlorinczcomi@gmail.com>
2025-11-22 18:27:42 -0500

    update

--- chatistician/client.py
+++ chatistician/client.py
@@ -26,7 +26,7 @@ name_prompt = client.recv(1024).decode()
 print(name_prompt, end="")
 name = input()
 client.sendall(name.encode())
-header.welcome(client_name)
+header.welcome(name)
 
 # defining breaking criteria
 breakers = config['breakers']['values']
