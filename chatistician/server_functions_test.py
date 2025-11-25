ef90e55537dce37262baf2aa2d8796b6c5729228
Noah Lorincz-Comi <noahlorinczcomi@gmail.com>
2025-11-24 22:56:45 -0500

    update

--- chatistician/server_functions_test.py
+++ chatistician/server_functions_test.py
@@ -17,7 +17,7 @@ import sys
 #     sys.stdout.write("\033[u")
 #     sys.stdout.flush()
 
-def redraw_header(text, fg_color=34, bg_color=47):
+def redraw_header(text='Type "!help" anytime to view help screen', fg_color=34, bg_color=47):
     """
     Redraw persistent header at the top with optional colors.
     
