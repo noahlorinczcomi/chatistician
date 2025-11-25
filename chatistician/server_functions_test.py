845fe80fc034024ff572b50331970d5c0c75043a
Noah Lorincz-Comi <noahlorinczcomi@gmail.com>
2025-11-24 22:00:18 -0500

    update

--- chatistician/server_functions_test.py
+++ chatistician/server_functions_test.py
@@ -23,7 +23,8 @@ def receive_msg(
 
             # re-prompt for client
             # print(f"{colored_server_name} ", end="", flush=True)
-            print(f"{colored_server_name} {msg}", flush=True)
+            print(f"{colored_client_name} {msg}", flush=True)
+
             utils_test.draw_footer()
             
             if msg.lower() in breakers:
